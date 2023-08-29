b93d777036722cdd149529600cb7e72e1ca821c2
Aldo2303 <aldod03@gmail.com>
2023-08-29 04:51:12 -0700

    task2 and task 3 made

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type-annotated function floor which takes a float n as argument
+Method: floor
+Returns: floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """floor of the float"""
+    return int(n)
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type-annotated function to_str that takes a float n as argument
+Method: to_str
+Returns: string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """string representation of the float"""
+    return str(n)
