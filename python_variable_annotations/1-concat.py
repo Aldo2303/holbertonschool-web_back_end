5452ad68e3e1c793f256fc43ff000b789b04f82a
Aldo2303 <aldod03@gmail.com>
2023-08-29 04:42:28 -0700

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""type-annotated funct concat that takes a string str1 and a string str2"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat two strings"""
+    return f"{str1}{str2}"
