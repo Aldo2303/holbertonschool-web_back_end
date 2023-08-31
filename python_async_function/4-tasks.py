6f012898cd5e254c9182deb5c75f4bead3683bb2
Aldo2303 <aldod03@gmail.com>
2023-08-31 08:08:58 -0700

    pycode

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -6,6 +6,7 @@ The code is nearly identical to wait_n except task_wait_random is being called.
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
     """Return list of all delays (float values) in ascending order"""
     delays = []
