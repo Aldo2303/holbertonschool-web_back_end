a304bd4db687cb18f0326051fb9b589ed706f615
Aldo2303 <aldod03@gmail.com>
2023-08-31 08:07:11 -0700

    task4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Take the code from wait_n and alter it into a new function task_wait_n.
+The code is nearly identical to wait_n except task_wait_random is being called.
+"""
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+async def task_wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
+    """Return list of all delays (float values) in ascending order"""
+    delays = []
+    for i in range(n):
+        delays.append(await task_wait_random(max_delay))
+    return sorted(delays)
