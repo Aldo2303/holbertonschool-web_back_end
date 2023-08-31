efcd6fb10edbe0a7bead2c28eda064deb204ecc8
Aldo2303 <aldod03@gmail.com>
2023-08-31 06:10:15 -0700

    task 1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Import wait_random from the previous python file that you’ve written and write
+an async routine called wait_n that takes in 2 int arguments
+(in this order): n and max_delay.
+Return: list of all the delays
+"""
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
+    """Return list of all delays (float values) in ascending order"""
+    delays = []
+    for i in range(n):
+        delays.append(await wait_random(max_delay))
+    delays.sort()
+    return delays
