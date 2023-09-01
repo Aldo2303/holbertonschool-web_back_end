8fdb0c515edf3dca2242cb9d537239f28a497d77
Aldo2303 <aldod03@gmail.com>
2023-09-01 09:57:34 -0700

    removed loop for exterior

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -12,6 +12,17 @@ async def async_comprehension() -> List[float]:
     return : 10 random numbers
     """
     list = []
+    list = [i async for i in async_generator()]
+    return list
+    
+    
+    """list = []
     async for i in async_generator():
         list.append(i)
     return list
+    This generated the following error:
+    "File contains a list comprehension".
+    This error is due to your improper usage of an async for loop within
+    another regular for loop in your async_comprehension function. In Python,
+    you cannot nest comprehension loops, whether they are regular
+    or asynchronous, in this manner."""
