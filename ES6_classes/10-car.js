e87de8663c506ecb3d112d59122386c1b393edd6
Aldo2303 <aldod03@gmail.com>
2023-09-17 15:56:37 -0700

    fix typerror and return only constructor

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -2,27 +2,12 @@
 
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string' || typeof motor !== 'string' || typeof color !== 'string') {
-      throw TypeError('Attributes must be strings');
-    }
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  get brand() {
-    return this._brand;
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
-  get color() {
-    return this._color;
-  }
-
   cloneCar() {
-    return new (this.constructor)(this._brand, this._motor, this._color);
+    return new this.constructor();
   }
 }
